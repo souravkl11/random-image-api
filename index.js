f7c90246e914b7bea67d00dfa19c12a15710e93e
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-22 15:02:51 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+image_array = [
+  'robo (1).jpg',
+  'robo (2).jpg',
+  'robo (3).jpg',
+  'robo (4).jpg',
+  'robo (5).jpg',
+  'robo (6).jpg',
+  'robo (7).jpg'
+]
+
+function get_random_image(){
+  // Get a random index
+  random_index = Math.floor(Math.random() * image_array.length);
+
+  // Get an image at the random_index
+  selected_image = image_array[random_index]
+
+  // Display the image
+  document.getElementById('image_shower').src = `./images/${selected_image}`
+}
